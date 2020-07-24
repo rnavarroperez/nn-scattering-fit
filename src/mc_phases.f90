3ae0583012e6f259cf773dd3dc6b218e29ed1be0
Rodrigo Navarro Perez <rnavarroperez@sdsu.edu>
2020-07-24 16:26:44 -0700

    Only write mc phases for the 'canonical' energies
    Signed-off-by: Rodrigo Navarro Perez <rnavarroperez@sdsu.edu>

--- src/mc_phases.f90
+++ src/mc_phases.f90
@@ -16,12 +16,13 @@ implicit none
 type(nn_model) :: ds_model
 real(dp), allocatable, dimension(:, :) :: mc_lambas
 real(dp), allocatable, dimension(:) :: parameters
-integer :: itlab
+integer, dimension(1:11) :: itlab = [1, 5, 10, 25, 50, 100, 150, 200, 250, 300, 350]
+integer :: i
 
 call set_ds_potential('ds_ope30_fff', ds_model, parameters)
 call read_montecarlo_parameters(abs(parameters) > 0, 'mc_parameters/mc_lambdas_fixedZ_0001_1000ope30fff.dat', mc_lambas)
-do itlab = 1, 350
-    call write_montecarlo_phases(ds_model, mc_lambas, 'np', itlab)    
+do i = 1, size(itlab)
+    call write_montecarlo_phases(ds_model, mc_lambas, 'np', itlab(i))    
 enddo
     
 end program write_mc_phases
