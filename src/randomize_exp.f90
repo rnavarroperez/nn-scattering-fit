fa16f3356d2f064ac5efbb2ef2fdb4922ebcb29b
Marielle Duran <meduran@sdsu.edu>
2021-03-23 23:20:22 -0700

    adds a function to randomize a database and the main program was cleaned up

--- src/randomize_exp.f90
+++ src/randomize_exp.f90
@@ -4,7 +4,9 @@
 !! Module to randomize experimental point values to create new
 !! experimental points. These points are built to follow the same
 !! pattern as old experimental values from the Granada database.
-!!
+!! This module also randomizes databases using the function written
+!! to randomize single experiments.      
+!!      
 !! @author      Marielle Elizabeth Duran
 !! 
       module randomize_exp
@@ -13,45 +15,67 @@
                 use random_num, only: box_muller_num
                 implicit none
                 private
-                public randomize_experiment, calc_mean, calc_stan_dev
+                public randomize_experiment, randomize_database
                 contains
 
 !!
 !> @brief       Creates new experimental values based on old points
 !!
-!! Function to form new eperimental point values based on old
-!experimental point values and the random number generator subroutine.
-! ADD MORE HERE 
+!! Function to form new experimental point values based on old
+!! experimental point values and the random number generator subroutine.
+!! The values for the old experimental point values are stored in the
+!! array, point_values. The subroutine for the random number generator
+!! is called within the do loop. New experimental point values are
+!! calculated using the mean, the randomly generated number, and
+!! standard deviation.
+!!
+!! @author      Marielle Elizabeth Duran
+!!
                 function randomize_experiment(old_exp) result(new_exp)
                         implicit none
                         type(nn_experiment), intent(in) :: old_exp
                         type(nn_experiment) :: new_exp
-                        integer :: i, length
+                        integer :: i
                         real(dp) :: num, mean, stan_dev
                         real(dp), dimension(SIZE(old_exp%data_points)) &
-                                :: point_values
-                        do i = 1, length
-                           point_values(i) = old_exp%data_points(i)%value
-                        end do
-                        mean = calc_mean(point_values)
-                        stan_dev = calc_stan_dev(point_values)
+                                :: point_values !< array for old values
                         new_exp = old_exp
-                        do i = 1, old_exp%n_data 
+                        do i = 1, old_exp%n_data
+                                mean = old_exp%data_points(i)%value
+                             stan_dev =old_exp%data_points(i)%stat_error
+                           point_values(i) = old_exp%data_points(i)%value
                         call box_muller_num(num)
                            new_exp%data_points(i)%value = &
-                                        old_exp%data_points(i)%value&
-                                        + num*stan_dev
+                                        mean + (num*stan_dev)
                         end do
-                        new_exp = new_exp 
                 end function randomize_experiment
+
+!!
+!> @brief       Creates a database with randomly generated numbers
+!!
+!! Function to randomize a data base using the function,
+!! randomize_experiment. That function randomizes a single experiment
+!! and randomize_database puts each experiment into an array that 
+!! represents a database.
+!!
+!! @author      Marielle Elizabeth Duran
+!! 
+                function randomize_database(old_data) result(new_data)
+                        implicit none
+               type(nn_experiment), dimension(:), intent(in) :: old_data
+              type(nn_experiment), allocatable, dimension(:) :: new_data
+                        integer :: i
+                        allocate(new_data(1:SIZE(old_data)))
+                        do i = 1, SIZE(old_data)
+                         new_data(i) = randomize_experiment(old_data(i))
+                        end do
+                end function randomize_database
 
 !!
 !> @brief       Calculates mean value
 !!
-!! Function to calculate the mean of a generic array. This function is
-!! used above with a more specific array in order to help find new
-!! experimental points that are similar to points in the Granada
-!! database.
+!! Function to calculate the mean of a generic array. This function was
+!! not used but was saved.
 !!
 !! @author      Marielle Elizabeth Duran
 !!  
